991fa1181b5af03b924f034c0f2cdf857095c10a
Kevin <kevin78515@gmail.com>
2017-03-26 04:12:26 +0800

    修改範例程式

--- playground/examples/_pump.js
+++ playground/examples/_pump.js
@@ -7,6 +7,10 @@ var slime = Game.createSprite("images/slime.gif");
 slime.x = 320;
 slime.y = 240;
 
+slime.when("click",function(){
+  	slime.scale*=0.5;
+});
+
 slime.when("hover",function(){
 	slime.scale+=0.1;
 });
