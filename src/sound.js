c10351fc95af97071649db6c84e46a1c83ea9b92
pika <owpika@gmail.com>
2017-05-16 14:35:21 +0800

    增加聲音播放功能

--- src/sound.js
+++ src/sound.js
@@ -1,27 +1,28 @@
-var loader = new(require('./loader'));
-
 function Sound (sounds, debugMode){
     this.sounds = sounds;
     this.playing = [];
+    this.muted = false;
+    this.volume = 1;
 }
 
 Sound.prototype = {
-    
-    preload: function (paths, completeFunc, progressFunc) {
-        this.sounds = loader.preload(paths, completeFunc, progressFunc);
-    },
 
-    play: function(url) {
+    play: function(url, loop) {
+        var audio;
         if(this.sounds[url]) {
-            var audio = this.sounds[url].cloneNode();
+            audio = this.sounds[url].cloneNode();
             this.playing.push(audio);
             audio.play();
         } else {
-            var audio = new Audio(url);
+            audio = new Audio(url);
             this.sounds[url] = audio;
             this.playing.push(audio);
             audio.play();
         }
+        audio.loop = loop;
+        audio.muted = this.muted;
+        audio.volume = this.volume;
+        return audio;
     },
 
     stop: function() {
@@ -29,6 +30,26 @@ Sound.prototype = {
             this.playing[i].pause();
         }
         this.playing = [];
+    },
+
+    mute: function(bool) {
+        this.muted = bool;
+        for(var i=0; i< this.playing.length; i++) {
+            this.playing[i].muted = bool;
+        }
+    },
+
+    setVolume: function(v) {
+        this.volume = v;
+        for(var i=0; i< this.playing.length; i++) {
+            this.playing[i].volume = v;
+        }
+    },
+
+    each: function(fn) {
+        for(var i=0; i< this.playing.length; i++) {
+            fn(this.playing[i]);
+        }
     }
 }
 
