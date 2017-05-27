adfdbd48659bb975f9a0a5840ba9b29879b94d69
pika <owpika@gmail.com>
2017-05-27 23:56:20 +0800

    繪圖增加三角形&多邊形

--- src/pen.js
+++ src/pen.js
@@ -12,30 +12,50 @@ Pen.prototype = {
 
         for(var i=0; i<this.shapes.length; i++) {
             var s = this.shapes[i];
-            if(s.type == 'text') {
+            if (s.type == 'text') {
                 ctx.textBaseline = "top";
                 ctx.font = s.size + "px " + s.font;
                 ctx.fillStyle = s.color;
                 ctx.fillText(s.t, s.x, s.y);
             }
-            if(s.type == 'rect') {
-                ctx.fillStyle = s.color;
-                ctx.fillRect(s.x, s.y, s.w, s.h);
+            else if (s.type == 'line') {
+                ctx.beginPath();
+                ctx.strokeStyle = s.color;
+                ctx.lineWidth = s.width;
+                ctx.moveTo(s.x1,s.y1);
+                ctx.lineTo(s.x2,s.y2);
+                ctx.stroke();
+                ctx.closePath();
             }
-            if(s.type == 'circle') {
+            else if (s.type == 'circle') {
                 ctx.beginPath();
                 ctx.fillStyle = s.color;
                 ctx.arc(s.x, s.y, s.r, 0, 2 * Math.PI);
                 ctx.fill();
                 ctx.closePath();
             }
-            if(s.type == 'line') {
+            else if (s.type == 'triangle') {
                 ctx.beginPath();
-                ctx.strokeStyle = s.color;
+                ctx.fillStyle = s.color;
                 ctx.moveTo(s.x1,s.y1);
                 ctx.lineTo(s.x2,s.y2);
-                ctx.stroke();
+                ctx.lineTo(s.x3,s.y3);
                 ctx.closePath();
+                ctx.fill();
+            }
+            else if (s.type == 'rect') {
+                ctx.fillStyle = s.color;
+                ctx.fillRect(s.x, s.y, s.w, s.h);
+            }
+            else if (s.type == 'polygon') {
+                ctx.beginPath();
+                ctx.fillStyle = s.color;
+                ctx.moveTo(s.points[0],s.points[1]);
+                for(var i=2; i<s.points.length; i+=2) {
+                     ctx.lineTo(s.points[i],s.points[i+1]);
+                }
+                ctx.closePath();
+                ctx.fill();
             }
         }
 
@@ -76,6 +96,19 @@ Pen.prototype = {
         this.shapes.push(s);
     },
 
+    drawTriangle: function (x1, y1, x2, y2, x3, y3, color) {
+        var s = {};
+        s.x1 = x1;
+        s.y1 = y1;
+        s.x2 = x2;
+        s.y2 = y2;
+        s.x3 = x3;
+        s.y3 = y3;
+        s.color = color;
+        s.type = 'triangle';
+        this.shapes.push(s);
+    },
+
     drawRect: function (x, y, width, height, color) {
         var s = {};
         s.x = x;
@@ -84,6 +117,14 @@ Pen.prototype = {
         s.h = height;
         s.color = color;
         s.type = 'rect';
+        this.shapes.push(s);
+    },
+    
+    drawPolygon: function (points, color) {
+        var s = {};
+        s.points = points;
+        s.color = color;
+        s.type = 'polygon';
         this.shapes.push(s);
     }
 }
