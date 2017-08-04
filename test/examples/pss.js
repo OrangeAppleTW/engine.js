48fe4cc38bcd4a6bfba7a12527debaaa487f4a0a
vyytx <dobeender@gmail.com>
2017-08-04 10:40:38 +0800

    pss.js fix

--- test/examples/pss.js
+++ test/examples/pss.js
@@ -14,7 +14,6 @@ Game.forever(function(){
     Game.pen.fillColor = '#333333';
     Game.pen.size = 60;
     Game.pen.drawText(re, 128, 128);
-    if(f) return;
     if(key["1"]) 
         user.costumeId = 0;
     if(key["2"])
@@ -22,26 +21,31 @@ Game.forever(function(){
     if(key["3"])
         user.costumeId = 2;
     if(key["1"] || key["2"] || key["3"]){
-        f = true;
-        comp.costumeId = Math.floor(Math.random()*3);
-        var u = user.costumeId/1 + 1;
-        console.log(u);
-        var c = comp.costumeId/1 + 1;
-        console.log(c);
-        if(u == c){
-            re = "Tie!";
-        }else if(u < c){
-            if((c-u) == 1){
-                re = "Comp Win!";
-            }else{
-                re = "User Win!";
-            }
-        }else if(c < u){
-            if((u-c) == 1){
-                re = "User Win!";
-            }else{
-                re = "Comp Win!";
+        if(f){
+            f = false;
+            comp.costumeId = Math.floor(Math.random()*3);
+            var u = user.costumeId/1 + 1;
+            console.log(u);
+            var c = comp.costumeId/1 + 1;
+            console.log(c);
+            if(u == c){
+                re = "Tie!";
+            }else if(u < c){
+                if((c-u) == 1){
+                    re = "Comp Win";
+                }else{
+                    re = "User Win";
+                }
+            }else if(c < u){
+                if((u-c) == 1){
+                    re = "User Win";
+                }else{
+                    re = "Comp Win";
+                }
             }
         }
+    }
+    if(!(key["1"]) && !(key["2"]) && !(key["3"])){
+        f = true;
     }
 });
