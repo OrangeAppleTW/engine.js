351810329f3d1419a054cf01b58795d661dbbf10
JingShiang Yang <owpika@gmail.com>
2019-12-20 18:44:56 +0800

    add test case

---
+++ test/events/touchBox.js
@@ -0,0 +1,16 @@
+var s1 = Game.createSprite('assets/bird.png');
+var s2 = Game.createSprite('assets/bg.jpg');
+var s3 = Game.createSprite('assets/gameover.png');
+
+
+Game.forever(function() {
+    [s1, s2, s3].forEach((s, idx) => {
+        var box = s._touchSystem.getBoxOf(s);
+        s.x = 100 + idx*100
+        s.opacity = 0.5;
+        s.scale = 1;
+        s.direction++;
+        Game.pen.color = 'red';
+        Game.pen.drawRect(box.x - box.width/2, box.y - box.height/2, box.width, box.height);
+    });
+});
---
+++ test/events/touchsystem_touch.js
@@ -0,0 +1,37 @@
+var s1 = Game.createSprite('assets/gameover.png');
+var s2 = Game.createSprite('assets/gameover.png');
+
+s1.opacity = s2.opacity = 0.5;
+
+
+Game.forever(function() {
+
+    s2.moveTo(Game.cursor);
+
+    s1.direction += 0.2;
+    s2.direction -= 0.05;
+
+    var s1_box = s1._touchSystem.getBoxOf(s1);
+    var s2_box = s2._touchSystem.getBoxOf(s2);
+    
+    Game.pen.drawRect(s1_box.x - s1_box.width/2, s1_box.y - s1_box.height/2, s1_box.width, s1_box.height);
+    Game.pen.drawRect(s2_box.x - s2_box.width/2, s2_box.y - s2_box.height/2, s2_box.width, s2_box.height);
+    Game.pen.drawCircle(s1_box.x, s1_box.y, 10);
+    Game.pen.drawCircle(s2_box.x, s2_box.y, 10);
+
+
+    if (s1._touchSystem.AABB(s1_box, s2_box)) {
+        Game.pen.color = 'red';
+    } else {
+        Game.pen.color = 'blue';
+    }
+
+
+    if (s1._touchSystem.touched(s1, s2)) {
+        s2.opacity = 1;
+    } else {
+        s2.opacity = 0.5;
+    }
+
+    
+});
